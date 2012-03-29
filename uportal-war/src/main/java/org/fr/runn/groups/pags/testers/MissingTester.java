d8caa9a326010111f8ecca437d4223e8b14bff4c
Vincent Bonamy <vbonamy@gmail.com>
2012-03-29 11:38:48 +0200

    Add a pags attribute tester : MissingTester

---
+++ uportal-war/src/main/java/org/fr/runn/groups/pags/testers/MissingTester.java
@@ -0,0 +1,26 @@
+package fr.runn.groups.pags.testers;
+
+import org.jasig.portal.groups.pags.testers.BaseAttributeTester;
+import org.jasig.portal.security.IPerson;
+
+/**
+ * Tests whether the attribute is null 
+ */
+public class MissingTester extends BaseAttributeTester {
+
+    public MissingTester(String attribute, String test) {
+        super(attribute, test);
+    }
+
+    public boolean test(IPerson person) {
+        // Get the list of values for the attribute
+        Object[] vals = person.getAttributeValues(getAttributeName());
+
+        // No values, test passed
+        if (vals == null || vals.length == 0 || (vals.length == 1 && vals[0].toString().equals(""))) {
+            return true;
+        } else {
+	    return false;
+        }
+    }
+}
